b983c28229c4fb0798c7fbab447e64c2c195cbbb
Afroz Syed <s.afroz@oasissys.com>
2025-10-07 10:56:58 +0530

    Adding changes for sendMessage with new code changes

--- src/main/java/com/healthexpert/healthbot/service/TwilioService.java
+++ src/main/java/com/healthexpert/healthbot/service/TwilioService.java
@@ -10,39 +10,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class TwilioService {
 
-    @Value("${twilio.account.sid}")
-    private String accountSid;
-
-    @Value("${twilio.auth.token}")
-    private String authToken;
-
-    @Value("${twilio.whatsapp.number}")
-    private String fromNumber;
+    private final String acc_SID="AC3224a830f63b9feec7d264a3ae1fc5b9";
+    private final String acc_TOKEN="1582d9bf64dfc586362339d88568c7e1";
 
     @PostConstruct
     public void init() {
-        Twilio.init(accountSid, authToken);
+        System.out.println("++++++initializing twilio++++++");
+        Twilio.init(acc_SID, acc_TOKEN);
     }
 
     public void sendMessage(String to, String message) {
-        Message.creator(
-                new PhoneNumber("whatsapp:" + to),
-                new PhoneNumber("whatsapp:" + fromNumber),
+        Twilio.init(acc_SID, acc_TOKEN);
+        Message msg =Message.creator(
+                new com.twilio.type.PhoneNumber("whatsapp:" + to),
+                new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
                 message
         ).create();
+        System.out.println("====="+msg.getSid());
     }
 
     public void sendSandBoxMessage(String from, String helloThisIsTest) {
-        String acc_SID="AC3224a830f63b9feec7d264a3ae1fc5b9";
-        String acc_TOKEN="21b970fd8376c218b33d9dd5bb25acf0";
-
-        Twilio.init(acc_SID,acc_TOKEN);
-        Message msg = Message.creator(
-        new com.twilio.type.PhoneNumber("whatsapp:+918096141026"),
-        new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
-        helloThisIsTest).create();
-
-        System.out.println("====="+msg.getSid());
 
     }
 }
